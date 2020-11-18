75f35042ccc222974e457bcec68bb11d79fea692
Chris Bowie <bowiemeister@gmail.com>
2020-11-17 19:59:17 -0500

    include colors

--- clonesetup.sh
+++ clonesetup.sh
@@ -2,8 +2,30 @@
 #bash script to reconfigure some stuff for cloned ubuntu vm's
 #constant variables
 pref_tz="America/New_York";
-wget "https://raw.githubusercontent.com/ChrisThePCGeek/bash-scripts/main/shell_colors";
-. shell_colors;
+
+#these are constants for the color escape sequences
+#======================================
+# Reset
+NC='\033[0m'       # Text Reset
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+#=======================================
 
 #remove and re-create ssh host keys
 echo -e "${BYellow}[INFO]${BGreen} removing host ssh keys${NC}";
@@ -21,19 +43,19 @@ then
 	dpkg-reconfigure tzdata;
 	cat /etc/timezone;
 else
-	echo -e "${BYellow}[INFO]${BGreen} Current time zone is set to preferred,${NC} $cur_tz";
+	echo -e "${BYellow}[INFO]${BGreen} Current time zone is set to preferred,${BRed} $cur_tz ${NC}";
 fi
 echo "================"
 echo
 #set new local host name
 echo -e "${BYellow}[INFO]${BGreen} Reading current hostname from file${NC}";
 read oldhost < /etc/hostname
-echo -e "${BYellow}[INFO]${BGreen} Current host name is : $oldhost${NC}";
+echo -e "${BYellow}[INFO]${BGreen} Current host name is : ${BRed}$oldhost${NC}";
 echo "."
-echo -e "${BYellow}[INFO]${BGreen} Setting new hostname";
+echo -e "${BYellow}[INFO]${BGreen} Setting new hostname${NC}";
 echo -n "  Please enter new hostname: ";
 read hostnm;
-echo -e "${BYellow}[INFO]${BGreen} Host name being set to $hostnm...${NC}";
+echo -e "\n${BYellow}[INFO]${BGreen} Host name being set to ${BRed}$hostnm...${NC}";
 hostnamectl set-hostname $hostnm;
 echo -e "${BYellow}[INFO]${BGreen} updating /etc/hosts...${NC}";
 sed -i "s/$oldhost/$hostnm/" /etc/hosts;
@@ -42,8 +64,9 @@ echo;
 echo "...done.";
 
 echo "Hosts file printout:";
-echo "====================";
+echo "====================${BCyan}";
 cat /etc/hosts;
+echo "${NC}";
 
 #run apt updates?
 echo -n "Do you want to run apt updates now? (Y/n";
@@ -52,11 +75,11 @@ read -n 1 answer;
 if [ -z "$answer" ] || [ "$answer" = "y" ] || [ "$answer" = "Y" ]
 then
 	#run updates
-	echo -e "${BYellow}[INFO]${BGreen} Running updates...${NC}";
+	echo -e "\n${BYellow}[INFO]${BGreen} Running updates...${NC}";
 	apt update && apt upgrade -y;
 else
 	#dont run updates
-	echo -e "${BYellow}[INFO]${BGreen} skipping updates.${NC}";
+	echo -e "\n${BYellow}[INFO]${BGreen} skipping updates.${NC}";
 fi
 
 #reset answer variable
@@ -69,9 +92,9 @@ read -n 1 answer;
 if [ -z "$answer" ] || [ "$answer" = "y" ] || [ "$answer" = "Y" ]
 then
 	#run updates
-	echo -e "${BYellow}[INFO]${BGreen} rebooting...${NC}";
+	echo -e "\n${BYellow}[INFO]${BGreen} rebooting...${NC}";
 	reboot;
 else
 	#dont run updates
-	echo -e "${BYellow}[INFO]${BGreen} not rebooting. Please remember to reboot to apply changes just made.${NC}";
+	echo -e "\n${BYellow}[INFO]${BGreen} not rebooting. Please remember to reboot to apply changes just made.${NC}";
 fi
