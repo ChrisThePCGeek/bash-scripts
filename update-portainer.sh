e8b85aa0b9e220f599fdbfa4d49eb1e10dfed7c4
Chris Bowie <bowiemeister@gmail.com>
2021-08-15 18:38:53 -0400

    update portainer script

---
+++ update-portainer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker stop portainer
+docker rm portainer
+docker pull portainer/portainer-ce
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portai$
