5a18e1e3e6bc94d9ac98d5e4cc751741ef9f8314
Chris Bowie <bowiemeister@gmail.com>
2021-10-15 14:49:04 -0400

    adding 9443 to pub port per new portainer w ssl

--- update-portainer.sh
+++ update-portainer.sh
@@ -3,4 +3,4 @@
 docker stop portainer
 docker rm portainer
 docker pull portainer/portainer-ce
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
